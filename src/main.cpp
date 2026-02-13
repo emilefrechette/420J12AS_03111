2c76f252af071a1e9c70b72a3a1549456c07f626
l-t-m-f <letthemicefree@gmail.com>
2026-02-13 13:18:42 -0500

    Premier commit - Exemple Import/Manip de textures.

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * Démo - Importation et utilisation de textures.
+ */
+
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+
+Sint32
+main (Sint32 argc, char *argv[])
+{
+	SDL_Log ("hello, SDL!");
+	return 0;
+}
